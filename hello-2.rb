b9c2cc0fff1b87ccb753513282bb98d24f65f895
愿山聖 <asjstksmr@gmail.com>
2021-02-14 14:57:39 +0900

    [habit]

---
+++ hello-2.rb
@@ -0,0 +1,25 @@
+puts 5
+puts "5"
+puts "5 + 3"
+puts "5+3"
+puts "私の年齢は" + 23.to_s + "です。"
+puts 100 + 200
+puts 100.to_s + "200"
+puts 100 + "100".to_i
+
+
+
+# 4章
+puts "abcdefg".length
+puts "私は今日も元気です。".include?("も")
+
+
+# 5章
+DWC = "DmmWebCamp"
+puts DWC
+eee = "akakakaka"
+puts eee
+DWC = "123"
+eee = "vvv"
+puts DWC
+puts eee
